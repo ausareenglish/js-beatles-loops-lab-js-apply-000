ca3636a278249d92347c29b16db52607ad09e321
Tehuti English <ausareenglish@gmail.com>
2020-03-28 02:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// add solution here
+// solution here
+function theBeatlesPlay(musicians, instruments) {
+  // declaring and assigning empty array
+  var beatlesPlay = [];
+  // 'for loop' that iterates over array of musicians
+  for (let i = 0; i < musicians.length; i++ ) {
+      //create a new string from musician and instrument arrays
+      
+      beatlesPlay.push(`${musicians[i]} plays ${instruments[i]}` )
+  }
+  // return the array of new array of strings containing what instruments each
+  // musician plays
+  
+  return beatlesPlay;
+}
+
+function johnLennonFacts (lennonFacts) {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  function lennonCounter () {
+      let i = 0;
+      i++;
+}
+  while (i < facts.length) {
+  
+  facts.push(...lennonFacts[i], [])
+  
+  
+  
+  
+}
